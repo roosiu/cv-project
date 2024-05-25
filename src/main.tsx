b35e2b44e6aafb34a19ed354e75737737f5ee4a2
roosiu <roosiu@gmail.com>
2024-05-25 23:56:37 +0200

    added icon and style scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <section>
+        <div className="left-sidebar">dsfdsa</div>
+        <div className="right-sidebar">dsad</div>
+      </section>
+    </>
   </React.StrictMode>
 );
